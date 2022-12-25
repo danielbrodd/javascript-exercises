389148a7f9ac1db66616c40e1e84d810b1396369
danielbrodd <daniel.brodd@gmail.com>
2022-12-25 22:34:48 +0100

    pass all tests for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+  for (const e of a) {
+    total += e
+  };
+  return parseInt(total);
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let sum = 1;
 
+  a.forEach(element => {
+    sum *= element
+  });
+  return sum;
+  };
+
+const power = function(a, b) {
+	return a**b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  }else{
+	for (let i = num -1; i >= 1 ; i--) {
+    num *= i
+  } return num;}
 };
 
 // Do not edit below this line
