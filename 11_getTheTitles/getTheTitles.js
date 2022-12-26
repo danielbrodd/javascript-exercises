bf4bafcdf801a3c414caca00988b3ead7a234059
danielbrodd <daniel.brodd@gmail.com>
2022-12-26 12:07:26 +0100

    Optimize function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookTitles = books.map(function(book){
+        const key =Object.values(book)[0]
+        return key
+    })
+    return bookTitles
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
