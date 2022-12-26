f6c9393fa20877d1039e2d7383b4846526885020
danielbrodd <daniel.brodd@gmail.com>
2022-12-26 09:05:39 +0100

    Solve problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const noCommastr = str.replace(/[^|\w\']/g, '');
+    
+    const noCommaArray = noCommastr.split('');
+  
+   
+    const reversed = noCommaArray.reverse().join('');
+    
 
+    if ( noCommastr.toUpperCase() == reversed.toUpperCase()){
+        return true;
+    }else return false;
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
